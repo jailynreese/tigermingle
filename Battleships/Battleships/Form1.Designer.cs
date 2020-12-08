be0054ccf56b29d1ed6b13788af2ff26fef4e342
Lemento <dagai89@gmail.com>
2020-12-07 18:14:24 -0800

    Added BatttleShips

---
+++ Battleships/Battleships/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Battleships
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carrierButton = new System.Windows.Forms.Button();
+            this.battleShipButton = new System.Windows.Forms.Button();
+            this.destroyerButton = new System.Windows.Forms.Button();
+            this.submarineButton = new System.Windows.Forms.Button();
+            this.patrolBoatButton = new System.Windows.Forms.Button();
+            this.playerLogRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // carrierButton
+            // 
+            this.carrierButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carrierButton.Location = new System.Drawing.Point(12, 111);
+            this.carrierButton.Name = "carrierButton";
+            this.carrierButton.Size = new System.Drawing.Size(100, 30);
+            this.carrierButton.TabIndex = 0;
+            this.carrierButton.Text = "Carrier";
+            this.carrierButton.UseVisualStyleBackColor = true;
+            // 
+            // battleShipButton
+            // 
+            this.battleShipButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.battleShipButton.Location = new System.Drawing.Point(12, 147);
+            this.battleShipButton.Name = "battleShipButton";
+            this.battleShipButton.Size = new System.Drawing.Size(100, 30);
+            this.battleShipButton.TabIndex = 1;
+            this.battleShipButton.Text = "BattleShip";
+            this.battleShipButton.UseVisualStyleBackColor = true;
+            // 
+            // destroyerButton
+            // 
+            this.destroyerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.destroyerButton.Location = new System.Drawing.Point(12, 183);
+            this.destroyerButton.Name = "destroyerButton";
+            this.destroyerButton.Size = new System.Drawing.Size(100, 30);
+            this.destroyerButton.TabIndex = 2;
+            this.destroyerButton.Text = "Destroyer";
+            this.destroyerButton.UseVisualStyleBackColor = true;
+            // 
+            // submarineButton
+            // 
+            this.submarineButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submarineButton.Location = new System.Drawing.Point(12, 219);
+            this.submarineButton.Name = "submarineButton";
+            this.submarineButton.Size = new System.Drawing.Size(100, 30);
+            this.submarineButton.TabIndex = 3;
+            this.submarineButton.Text = "Submarine";
+            this.submarineButton.UseVisualStyleBackColor = true;
+            // 
+            // patrolBoatButton
+            // 
+            this.patrolBoatButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.patrolBoatButton.Location = new System.Drawing.Point(12, 255);
+            this.patrolBoatButton.Name = "patrolBoatButton";
+            this.patrolBoatButton.Size = new System.Drawing.Size(100, 30);
+            this.patrolBoatButton.TabIndex = 4;
+            this.patrolBoatButton.Text = "Patrol Boat";
+            this.patrolBoatButton.UseVisualStyleBackColor = true;
+            // 
+            // playerLogRichTextBox
+            // 
+            this.playerLogRichTextBox.Location = new System.Drawing.Point(12, 517);
+            this.playerLogRichTextBox.Name = "playerLogRichTextBox";
+            this.playerLogRichTextBox.Size = new System.Drawing.Size(776, 69);
+            this.playerLogRichTextBox.TabIndex = 5;
+            this.playerLogRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 598);
+            this.Controls.Add(this.playerLogRichTextBox);
+            this.Controls.Add(this.patrolBoatButton);
+            this.Controls.Add(this.submarineButton);
+            this.Controls.Add(this.destroyerButton);
+            this.Controls.Add(this.battleShipButton);
+            this.Controls.Add(this.carrierButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button carrierButton;
+        private System.Windows.Forms.Button battleShipButton;
+        private System.Windows.Forms.Button destroyerButton;
+        private System.Windows.Forms.Button submarineButton;
+        private System.Windows.Forms.Button patrolBoatButton;
+        private System.Windows.Forms.RichTextBox playerLogRichTextBox;
+    }
+}
+
---
+++ Battleships/Battleships/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleships
+{
+    public partial class Form1 : Form
+    {
+        public Button[,] board;
+        public class Ship
+        {
+            public Ship(string Name, int Size)
+            {
+                this.ShipName = Name;
+                this.Size = Size;
+            }
+            private string name;
+            public string ShipName
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            private int size;
+            public int Size
+            {
+                get { return size; }
+                set { Size = value; }
+            }
+        }
+
+        public Ship selectedShip = null;
+        public Ship[] ships;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            CreateButtons();
+            CreateShips();
+            //selectingShip();
+        }
+
+        private void CreateButtons()
+        {
+            board = new Button[10, 10];
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    board[x, y] = new Button();
+                    board[x, y].BackColor = System.Drawing.Color.White;
+                    board[x, y].Location = new Point(150 + 50*x, 10 + 50*y);
+                    board[x, y].Height = 50;
+                    board[x, y].Width = 50;
+                    board[x, y].Text = x.ToString() + ":" + y.ToString();
+                    board[x, y].Name = x.ToString() + ":" + y.ToString() + "Button";
+
+                    board[x, y].Click += new EventHandler(Tile__Click);
+                    board[x, y].MouseEnter += new EventHandler(Tile__MouseEnter);
+                    board[x, y].MouseLeave += new EventHandler(Tile__MouseLeave);
+
+                    Controls.Add(board[x, y]);
+                }
+            }
+        }
+
+        private void CreateShips()
+        {
+            ships = new Ship[5];
+            //playerLogRichTextBox.Text = ships.ToString();
+            /*
+            ships[0] = new Ship(carrierButton.Text, 5);
+            ships[1] = new Ship(battleShipButton.Text, 4);
+            ships[2] = new Ship(destroyerButton.Text, 3);
+            ships[3] = new Ship(submarineButton.Text, 3);
+            ships[4] = new Ship(patrolBoatButton.Text, 2);*/
+        }
+
+        private void Tile__Click(object sender, EventArgs e)
+        {
+            Button butt = (Button)sender;
+
+            butt.BackColor = System.Drawing.Color.Blue;
+            butt.ForeColor = System.Drawing.Color.Green;
+        }
+
+        private void Tile__MouseEnter(object sender, EventArgs e)
+        {
+            Button butt = (Button)sender;
+
+            int x = (butt.Location.X - 150) / 50;
+            int y = (butt.Location.Y - 10) / 50;
+            playerLogRichTextBox.Text += x.ToString() + ":" + y.ToString() + " | ";
+
+            //playerLogRichTextBox.Text += "\nSelected: " + butt.Text;
+            if (selectedShip == null)
+            {
+                butt.BackColor = System.Drawing.Color.LightBlue;
+                for (int i = 0; i < 3; i++)
+                {
+                    try
+                    {
+                        board[x + i, y].BackColor = System.Drawing.Color.LightBlue;
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+                butt.BackColor = System.Drawing.Color.LightBlue;
+            }
+        }
+
+        private void Tile__MouseLeave(object sender, EventArgs e)
+        {
+            Button butt = (Button)sender;
+
+            butt.BackColor = System.Drawing.Color.White;
+        }
+
+        private void selectingShip()
+        {
+            carrierButton.Click += new EventHandler(ShipSelect);
+            battleShipButton.Click += new EventHandler(ShipSelect);
+            destroyerButton.Click += new EventHandler(ShipSelect);
+            submarineButton.Click += new EventHandler(ShipSelect);
+            patrolBoatButton.Click += new EventHandler(ShipSelect);
+        }
+
+        private void ShipSelect(object sender, EventArgs e)
+        {
+            Button shipButt = (Button)sender;
+            /*
+            selectedShip = null;
+            
+            foreach (Ship i in ships)
+            {
+                if (shipButt.Text == i.ShipName)
+                {
+                    selectedShip = i;
+                }
+            }*/
+
+            if (shipButt.Name == carrierButton.Name) carrierButton.Enabled = false;
+            else carrierButton.Enabled = true;
+            if (shipButt.Name == battleShipButton.Name) battleShipButton.Enabled = false;
+            else battleShipButton.Enabled = true;
+            if (shipButt.Name == destroyerButton.Name) destroyerButton.Enabled = false;
+            else destroyerButton.Enabled = true;
+            if (shipButt.Name == submarineButton.Name) submarineButton.Enabled = false;
+            else submarineButton.Enabled = true;
+            if (shipButt.Name == patrolBoatButton.Name) patrolBoatButton.Enabled = false;
+            else patrolBoatButton.Enabled = true;
+
+            playerLogRichTextBox.Text += "Selected: " +  shipButt.Text + "\n";
+        }
+    }
+}
